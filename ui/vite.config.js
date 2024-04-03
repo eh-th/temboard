5ec03c1304f1e29033bb7cbb9bef95e80e4337cf
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-04-03 10:41:06 +0200

    Upgrade monitoring page to Vue3
    Vuedraggable is not compatible with Vue3. We get rid of it for now and will
    find an alternative later on.
    
    Changes were made to DateRangePicker to prevent weird behavior. We now rely on
    router.isReady or router.afterEach instead of unreliable usage of watch.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -37,6 +37,7 @@ export default defineConfig({
         activity: "/activity.js",
         dashboard: "/dashboard.js",
         home: "/home.js",
+        monitoring: "/monitoring.js",
         temboard: "/temboard.js",
       },
     },
